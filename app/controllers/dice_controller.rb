841e2b6f894851d80c115cfc880bc81174cf8592
m-a-l <marie-alix.lesellier@simplybusiness.co.uk>
2023-07-27 11:47:14 +0100

    export traceparent to s3 metadata

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -13,6 +13,9 @@ class DiceController < ApplicationController
   end
 
   def upload_to_s3(file_name)
+    # binding.pry
+    target = {}
+    OpenTelemetry.propagation.inject(target)
     s3 = Aws::S3::Client.new(
       region: 'eu-west-1',
       endpoint: 'http://localhost:4566',
@@ -21,7 +24,22 @@ class DiceController < ApplicationController
       force_path_style: true
     )
     File.open("storage/#{file_name}", 'rb') do |file|
-      s3.put_object(bucket: 'dice-app-bucket', key: 'dice-roll-s3.txt', body: file)
+      s3.put_object(
+        bucket: 'dice-app-bucket',
+        key: 'dice-roll-s3.txt',
+        body: file,
+        metadata: {
+          # traceid: OpenTelemetry::Trace.current_span.context.trace_id,
+          traceparent: target['traceparent'],
+          # baggage: target['baggage'],
+          # tracestate: target['tracestate']
+        }
+      )
     end
+  end
+
+  def retrieve
+    metadata = s3.head_object(bucket: 'dice-app-bucket', key: 'dice-roll-s3.txt').metadata
+    context = OpenTelemetry.propagation.extract(metadata, OpenTelemetry::Context.empty)
   end
 end
