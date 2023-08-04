313c6cec8207acd76f8c6a265dda73383d2eee8f
m-a-l <marie-alix.lesellier@simplybusiness.co.uk>
2023-08-04 15:35:04 +0100

    try context propagation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "dice#index"
+  root 'dice#index'
   get '/fake_worker', to: 'dice#fake_worker'
 end
